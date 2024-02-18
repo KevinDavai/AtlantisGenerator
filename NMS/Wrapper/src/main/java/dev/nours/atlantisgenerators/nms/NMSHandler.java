f99d152077c4169c8dc591793752da51c8156bb4
Kevin Davailles <claragmtt@gmail.com>
2024-02-18 08:18:33 +0100

    Remove NMS

--- NMS/Wrapper/src/main/java/dev/nours/atlantisgenerators/nms/NMSHandler.java
+++
@@ -1,9 +0,0 @@
-package dev.nours.atlantisgenerators.nms;
-
-import org.bukkit.Chunk;
-import org.bukkit.World;
-import org.bukkit.block.Block;
-
-public interface NMSHandler {
-    boolean countBlockInChunk(Chunk world);
-}
--- src/main/java/dev/nours/atlantisgenerators/AtlantisGeneratorsPlugin.java
+++ src/main/java/dev/nours/atlantisgenerators/AtlantisGeneratorsPlugin.java
@@ -1,53 +1,24 @@
 package dev.nours.atlantisgenerators;
 
 import dev.nours.atlantisgenerators.managers.ConfigManager;
-import dev.nours.atlantisgenerators.nms.NMSAdapter;
-import dev.nours.atlantisgenerators.nms.NMSHandler;
-import net.minecraft.world.item.AirItem;
-import net.minecraft.world.level.block.Blocks;
-import org.bukkit.block.Block;
-import org.bukkit.Bukkit;
-import org.bukkit.Location;
-import org.bukkit.World;
-import org.bukkit.entity.Entity;
-import org.bukkit.entity.ItemFrame;
-import org.bukkit.entity.Player;
 import org.bukkit.event.EventHandler;
 import org.bukkit.event.Listener;
-import org.bukkit.event.player.PlayerInteractEntityEvent;
-import org.bukkit.event.player.PlayerInteractEvent;
 import org.bukkit.event.player.PlayerJoinEvent;
-import org.bukkit.event.world.ChunkLoadEvent;
-import org.bukkit.inventory.ItemStack;
 import org.bukkit.plugin.java.JavaPlugin;
 
 public final class AtlantisGeneratorsPlugin extends JavaPlugin implements Listener {
     private ConfigManager configManager;
-
     @Override
     public void onEnable() {
-        if (!NMSAdapter.isValidVersion()) {
-            this.getLogger().severe(String.format("AtlantisGenerators only supports %s through %s. The plugin has been disabled.", "1.16.5", "1.20.4"));
-            Bukkit.getPluginManager().disablePlugin(this);
-            return;
-        }
-
-        Bukkit.getPluginManager().registerEvents(this, this);
-
         configManager = new ConfigManager(this);
+        this.getServer().getPluginManager().registerEvents(this, this);
     }
 
     @EventHandler
-    public void onChunkLoad(ChunkLoadEvent chunk) {
-        boolean f = NMSAdapter.getHandler().countBlockInChunk(chunk.getChunk());
+    public void onPlayerJoin(PlayerJoinEvent e) {
+        String itemName = configManager.getItemsConfig().getItemName();
+        String itemLore = configManager.getItemsConfig().getItemLore();
+        e.getPlayer().sendMessage(itemName + "  dfdsfg");
+        e.getPlayer().sendMessage(itemLore);
     }
-
-//    @EventHandler
-//    public void paintingRotate(final PlayerInteractEvent event) {
-//        for(Entity entity : event.getClickedBlock().getLocation().getNearbyEntities(1,1,1)) {
-//            if(entity instanceof ItemFrame) {
-//                this.getLogger().info("item: " + entity.toString());
-//            }
-//        }
-//    }
 }
